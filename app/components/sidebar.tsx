496f837d6f40bc6067fc02288df5f3e08c6173fc
buoudidi <131969837+buoudidi@users.noreply.github.com>
2023-04-27 22:10:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -92,7 +92,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          客服微信 buou9919
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
